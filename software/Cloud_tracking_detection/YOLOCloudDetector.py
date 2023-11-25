d2d3f87c9e84580371d9aadcf21e57d38389215e
RichardGuo0301 <131337093+RichardGuo0301@users.noreply.github.com>
2023-11-25 15:28:19 -0800

    Update YOLOCloudDetector.py

--- software/Cloud_tracking_detection/YOLOCloudDetector.py
+++ software/Cloud_tracking_detection/YOLOCloudDetector.py
@@ -1,7 +1,6 @@
 import cv2
 from ultralytics import YOLO
 from picamera2 import Picamera2
-import numpy as np
 
 class YOLOCloudDetector:
     def __init__(self, model_path):
@@ -17,47 +16,33 @@ class YOLOCloudDetector:
         self.piCam.configure("preview")
         self.piCam.start()
 
-    def detect_clouds(self):
+    def detect_objects_and_clouds(self):
         # Capture a frame
         frame = self.piCam.capture_array()
 
         # Perform object detection using the YOLO model
         results = self.model(frame)
-
-        # Visualize the results on the frame
         annotated_frame = results[0].plot()
 
-        # Initialize cloud area count
-        cloud_area = 0
-        cloud_confidence_threshold = 0.6  # Confidence threshold for cloud detection
+        # Convert to grayscale for cloud detection
+        gray = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2GRAY)
+        ret, binary = cv2.threshold(gray, 175, 255, cv2.THRESH_BINARY)
 
-        # Iterate over detected objects
-        for r in results:
-            for box in r.boxes:
-                if self.model.names[int(box.cls)] == 'cloud' and box.conf > cloud_confidence_threshold:
-                    # Extract bounding box coordinates
-                    x1, y1, x2, y2 = box.xyxy
-                    # Print bounding box coordinates
-                    print(f"Cloud Bounding Box: ({x1:.2f}, {y1:.2f}), ({x2:.2f}, {y2:.2f})")
+        # Find contours of clouds
+        contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(annotated_frame, contours, -1, (0, 0, 255), 3)
 
-                    # Calculate the area of the box
-                    cloud_area += (x2 - x1) * (y2 - y1)
-
-        # Display the image with annotations
-        cv2.imshow("YOLOv8 Detection with Cloud Annotations", annotated_frame)
-
-        # Calculate and print cloud area ratio
-        total_area = frame.shape[0] * frame.shape[1]
-        cloud_area_ratio = cloud_area / total_area
-        print(f"Cloud area: {cloud_area}, Total area: {total_area}, Cloud area ratio: {cloud_area_ratio}")
+        # Display the image with cloud contours
+        cv2.imshow("YOLOv8 Detection with Cloud Contours", annotated_frame)
 
         # Check for exit condition
         if cv2.waitKey(1) & 0xFF == ord("q"):
             return
 
 # Usage
-model_path = '/home/pi/Desktop/cloud/best.pt'
+model_path = '/home/pi/Desktop/cloud/best.pt'  # Ensure this is the correct model path
 detector = YOLOCloudDetector(model_path)
 
+# Continuously detect objects and clouds
 while True:
-    detector.detect_clouds()
+    detector.detect_objects_and_clouds()
