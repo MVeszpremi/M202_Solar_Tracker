7e021f36201e4c471a662ebc2a648b65115d82fa
RichardGuo0301 <131337093+RichardGuo0301@users.noreply.github.com>
2023-12-01 00:21:53 -0800

    Update Main.py can successfully run

--- software/Full_System_Implementation/Main.py
+++ software/Full_System_Implementation/Main.py
@@ -2,43 +2,47 @@ from SunPositionDrawer import SunPositionDrawer
 from ArduinoSerialInterface import ArduinoSerialInterface, ArduinoSerialInterfaceTesting
 from CloudSegmentation import CloudSegmentation
 from WeatherChecker import WeatherChecker
-from WeatherPredictor import WeatherPredictor
+from WeatherPredictor import predict_weather
 import time
 import joblib
 import matplotlib.pyplot as plt
 import cv2
 
+
 class Main:
     def __init__(self, location_city, api_weather, svm_classifier):
         plt.ion()  # Enable interactive mode
         self.sun_drawer = SunPositionDrawer()
-        self.arduino_interface = ArduinoSerialInterfaceTesting() #ArduinoSerialInterface()
+        self.arduino_interface = ArduinoSerialInterfaceTesting()  # ArduinoSerialInterface()
         self.cloud_segmentation = CloudSegmentation()
         self.weather_checker = WeatherChecker(api_weather, location_city)
-        self.weather_predictor = WeatherPredictor(svm_classifier)
-
+        self.svm_classifier = joblib.load(svm_classifier_path)
 
     def periodic_task_10_second(self):
         print("10s timer")
         cv2.destroyAllWindows()
+
+        # Check the weather using the API
+        weather_data = self.weather_checker.get_weather()  # Corrected method name
+        api_bad_weather = self.weather_checker.is_bad_weather(weather_data)
+
+        # Use CloudSegmentation to capture and process clouds
         self.cloud_segmentation.capture_image()
         self.cloud_segmentation.processForClouds()
-        current_bad_weather = self.weather_checker.check_current_severe_weather()
-        ret, frame = self.weather_predictor.cap.read()
+
+        # Use the camera for weather prediction
+        ret, frame = self.cloud_segmentation.get_captured_image()
         if ret:
-            # The camera detects if the current weather is sunny
-            current_sunny_weather_cam = self.weather_predictor.predict_weather(frame) == "Sunny"
-
-            # If the API and camera test results do not match, the camera results will prevail
-            current_sunny_weather = current_sunny_weather_cam if current_sunny_weather_api != current_sunny_weather_cam else current_sunny_weather_api
+            weather = predict_weather(frame, self.svm_classifier)
+            print("Predicted weather:", weather)
+            camera_sunny_weather = weather == "Sunny"
         else:
-            # If it is not possible to read frames from the camera, use the API result
-            current_sunny_weather = current_sunny_weather_api
+            camera_sunny_weather = True  # Default to sunny if camera fails
 
-        if not current_sunny_weather:
+        # Decide the action based on API and camera weather data
+        if api_bad_weather and not camera_sunny_weather:
             self.arduino_interface.moveToAngle(-59.0, 0)
-            print("Non-sunny weather is currently present.")
-
+            print("Non-sunny or bad weather detected, adjusting solar panel.")
         else:
             self.arduino_interface.moveToAngle(self.sun_drawer.getRotX(), self.sun_drawer.getRotY())
             self.cloud_segmentation.setErrAngle(self.sun_drawer.getRotXErr(), self.sun_drawer.getRotYErr())
@@ -46,7 +50,6 @@ class Main:
     def periodic_task_2_second(self):
         print(f"CLAMPED: yaw (x):{self.sun_drawer.getRotX()}, pitch(y):{self.sun_drawer.getRotY()}")
         print(f"ERR    : yaw (x):{self.sun_drawer.getRotXErr()}, pitch(y):{self.sun_drawer.getRotYErr()}")
-       
 
     def run(self):
         last_called = time.time()
@@ -55,7 +58,7 @@ class Main:
         while True:
             cv2.waitKey(1)  # Waits 1ms
             self.arduino_interface.run()
-            if(self.arduino_interface.isDevReady()):
+            if (self.arduino_interface.isDevReady()):
                 self.sun_drawer.run()
                 if time.time() - last_called_2 > 2:
                     self.periodic_task_2_second()
@@ -68,5 +71,6 @@ class Main:
 if __name__ == "__main__":
     svm_classifier_path = './svm_classifier.joblib'  # update the path
     svm_classifier = joblib.load(svm_classifier_path)
-    main = Main(location_city='Los Angeles', api_weather = '32b8600ccd902c30801c6fe5ac806afa', svm_classifier=svm_classifier)
+    main = Main(location_city='Los Angeles', api_weather='32b8600ccd902c30801c6fe5ac806afa',
+                svm_classifier=svm_classifier)
     main.run()
